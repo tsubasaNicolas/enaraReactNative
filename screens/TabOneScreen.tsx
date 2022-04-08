e8754c10f6b093f7946f27fb0df02e99a5417e6c
Nicolás Carrasco <nicolas.carrasco.m@gmail.com>
2022-04-08 15:53:30 -0400

    modificar array diccionary

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,28 +1,169 @@
-import { StyleSheet } from 'react-native';
+import { Alert, GestureResponderEvent, ScrollView, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import { useEffect, useState } from 'react';
+
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+
+
+let  board = [
+  "L","I","S","T",
+  "O","F","A","T",
+  "S","T","R","S",
+  "O","R","A","Y"
+    ]
+  ;
+
+  const dictionary= [
+    "array",
+    "arrays",
+    "art",
+    "arts",
+    "bag",
+    "bee",
+    "bog",
+    "dog",
+    "fall",
+    "fast",
+    "fat",
+    "fell",
+    "fig",
+    "fill",
+    "fist",
+    "gap",
+    "hello",
+    "hoop",
+    "jack",
+    "job",
+    "jog",
+    "keg",
+    "kick",
+    "lift",
+    "lifts",
+    "lire",
+    "list",
+    "load",
+    "loaf",
+    "loan",
+    "loft",
+    "log",
+    "lost",
+    "lure",
+    "lust",
+    "man",
+    "mic",
+    "mice",
+    "milk",
+    "opal",
+    "pan",
+    "pee",
+    "pin",
+    "polk",
+    "poo",
+    "pop",
+    "rant",
+    "rat",
+    "rats",
+    "rent",
+    "rest",
+    "rust",
+    "sat",
+    "soft",
+    "sort",
+    "soy",
+    "start",
+    "starts",
+    "street",
+    "tar",
+    "tart",
+    "tarts",
+    "toy",
+    "toys",
+    "tray",
+    "trays"
+  ];
+
+  board = board.sort(() => Math.random() - 0.5)
+
+
+
+  const [lista, setLista] = useState(board);
+  const [word, setWord] = useState('');
+  const [count, setCount] = useState('')
+  const [dicc, setDicc] = useState(dictionary)
+
+
+  useEffect(() => {
+if(dicc.find(element => element == word.toLowerCase())== undefined){
+  console.log('palabra no encontrada')
+}
+else{
+ alert('Palabra ' + word + ' encontrada. Busca una nueva palabra');
+ setWord('');
+
+}
+    console.log('word '+ word);
+  }, [word])
+  
+
+
+const onPress2 = (y:string) => {
+  setWord(prevCount => prevCount + y);
+  console.log(count)
+}
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+    <ScrollView>
+    <View 
+   style={styles.container}
+    >
+    <View style={styles.grid}>
+      {
+       lista.map((x,i)=>(
+         <TouchableOpacity  onPress={() => onPress2(x)} key={i} >
+            <Text  style={styles.title}> {x}</Text>
+         </TouchableOpacity>
+        ))
+      }
+</View>
+
+</View>
+<View>
+  <TextInput value={word} style={{backgroundColor:'white', width:200,marginLeft:40, marginTop:40}}/>
+</View>
+    
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  grid:{
+    display:'flex',
+    flexWrap:'wrap',
+    flexDirection:'row',
+    justifyContent:'center',
+    alignItems: 'center',
+    width:'90%',
+    
+
+  },
+
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
   title: {
+    margin:4,
     fontSize: 20,
     fontWeight: 'bold',
+    width:70,
+    height:70,
+    //color:'tomato'
+    backgroundColor:'teal',
   },
   separator: {
     marginVertical: 30,
--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,14 +1,58 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TouchableOpacity } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function TabTwoScreen() {
+
+  var arr = [
+    "L","I","S","T",
+    "O","F","A","T",
+    "S","T","R","S",
+    "O","R","A","Y"
+  ]
+/*
+  function listToMatrix(list :Array<string>, elementsPerSubArray:number) {
+    var matrix = [], i, k;
+
+    for (i = 0, k = -1; i < list.length; i++) {
+        if (i % elementsPerSubArray === 0) {
+            k++;
+            matrix[k] = [];
+        }
+
+        matrix[k].push(list[i]);
+    }
+
+    return matrix;
+}
+*/
+ // var matrix = listToMatrix(arr, 4);
+
+  function getNeighbours(col:number, row:number)
+{
+  var w = 4;
+  
+  var start = (row > 0 ? row * w - w : row * w) + (col > 0 ? col - 1 : col);
+  var rowSpan = ((row > 0 ? 2 : 1) + (row < (w - 1) ? 1 : 0)) * w;
+  var colSpan = (col > 0 ? 2 : 1) + (col < (w -1) ? 1 : 0);
+  console.log('array '+ arr)
+  
+  var center = col + row * w;
+  var result = [];
+  for (var r = start; r < start + rowSpan; r += w)  
+    for (var i = r; i < r + colSpan; i++)
+      if (!(i === center))
+         result.push(arr[i]);
+      
+  return  console.log(result);
+ 
+}
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+      <TouchableOpacity onPress={()=>getNeighbours(1,1)} >
+      <Text  style={styles.title}>Find</Text>
+      </TouchableOpacity>
+  
     </View>
   );
 }
